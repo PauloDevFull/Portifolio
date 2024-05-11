65b1fa53725c1428fcb7b59b2a880e8ed9fcc09f
PauloDevFull <paulo.dev.full@gmail.com>
2024-05-11 15:10:09 -0300

    Construção do portifólio
    -adicionei algumas funcionalidade no meu hamburg
    -desativei o form
    -adicionei um javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("homebox").addEventListener("click", function() {
+    document.getElementById("menu-hamburg").checked = false;
+});
+
+document.getElementById("sobmim").addEventListener("click", function() {
+    document.getElementById("menu-hamburg").checked = false;
+});
+
+document.getElementById("proje").addEventListener("click", function() {
+    document.getElementById("menu-hamburg").checked = false;
+});
+
+document.getElementById("ctt").addEventListener("click", function() {
+    document.getElementById("menu-hamburg").checked = false;
+});
